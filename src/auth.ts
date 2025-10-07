e5134a161ae495876043b99e6cec45c20febb704
Fleming John <flemjohn08@gmail.com>
2025-10-07 09:33:51 +0000

    still facing the error

--- src/auth.ts
+++ src/auth.ts
@@ -10,6 +10,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     }),
   ],
   secret: process.env.AUTH_SECRET,
+  trustHost: true,
   callbacks: {
     async jwt({ token, account }) {
       if (account) {
